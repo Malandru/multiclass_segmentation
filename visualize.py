ddce0888badf80d03270111af1deaf9bc9276de3
Andres Martinez <andres.martinez@expertos-ti.com>
2021-03-03 10:55:05 -0600

    Visualize interface to name figures and use them in Console/Notebooks programs

---
+++ visualize.py
@@ -0,0 +1,17 @@
+import matplotlib.pyplot as plt
+
+
+def prepare_figure(**images):
+    """PLot images in one row."""
+    n = len(images)
+    plt.figure()
+    for i, (name, image) in enumerate(images.items()):
+        plt.subplot(1, n, i + 1)
+        plt.xticks([])
+        plt.yticks([])
+        plt.title(' '.join(name.split('_')).title())
+        plt.imshow(image)
+
+
+def show_prepared_figures():
+    plt.show()
