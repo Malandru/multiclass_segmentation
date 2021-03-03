c6733a600a289b7056ed5210bafd755e8bd456f4
Andres Martinez <andres.martinez@expertos-ti.com>
2021-03-03 11:49:18 -0600

    LR parameter is not used

--- unet/config.py
+++ unet/config.py
@@ -4,7 +4,6 @@ import segmentation_models as sm
 class Config:
     BACKBONE = 'efficientnetb3'
     BATCH_SIZE = 1
-    LR = 0.0001  # Optimizer parameter
 
     preprocess_input = sm.get_preprocessing(BACKBONE)
     activation = 'softmax'
